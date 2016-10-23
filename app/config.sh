ca707d89f6fde5c6b624327817c2ec8f8b7289f8
Pascal Grimaud <pascalgrimaud@gmail.com>
2016-10-23 17:59:00 +0200

    Initial commit: add travis build

---
+++ app/config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export JHIPSTER_REPO=https://github.com/jhipster/generator-jhipster.git
+export JHIPSTER_BRANCH=master
+export JHIPSTER_NODE_CACHE=1
+
+export PROFILE=dev
+export RUN_APP=1
+export PROTRACTOR=0
---
+++ travis/install/02-install-jhipster-stack.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -ev
+
+#-------------------------------------------------------------------------------
+# Update npm
+#-------------------------------------------------------------------------------
+npm install -g npm
+
+#-------------------------------------------------------------------------------
+# Install yeoman, bower and gulp
+#-------------------------------------------------------------------------------
+npm install -g yo bower gulp-cli
+
+#-------------------------------------------------------------------------------
+# Install JHipster
+#-------------------------------------------------------------------------------
+git clone $JHIPSTER_REPO generator-jhipster
+cd generator-jhipster
+if [ $JHIPSTER_BRANCH != "master" ]; then
+    git checkout $JHIPSTER_BRANCH $JHIPSTER_BRANCH
+fi
+npm install
+npm link
