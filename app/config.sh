f4557e1ffa9007ebe1775332b8a7f17bfa4495ee
Pascal Grimaud <pascalgrimaud@gmail.com>
2016-10-23 21:09:59 +0200

    Remove useless var, and put default node_module cache to 0

--- app/config.sh
+++ app/config.sh
@@ -3,8 +3,4 @@
 export JHIPSTER_REPO=https://github.com/jhipster/generator-jhipster.git
 # you can put: master, latest, v3.9.1, vX.Y.Z
 export JHIPSTER_BRANCH=latest
-export JHIPSTER_NODE_CACHE=1
-
-export PROFILE=dev
-export RUN_APP=1
-export PROTRACTOR=0
+export JHIPSTER_NODE_CACHE=0
