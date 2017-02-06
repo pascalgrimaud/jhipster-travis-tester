2317c4c92fe177ce36d527a74de09ecfd6627982
Pascal Grimaud <pascalgrimaud@gmail.com>
2017-02-06 08:10:14 +0100

    Try to reproduce issue 5119

--- app/config.sh
+++ app/config.sh
@@ -2,4 +2,4 @@
 
 export JHIPSTER_REPO=https://github.com/jhipster/generator-jhipster.git
 # you can put: master, latest, v3.12.1, vX.Y.Z
-export JHIPSTER_BRANCH=master
+export JHIPSTER_BRANCH=v4.0.1
--- app/test.sh
+++ app/test.sh
@@ -17,26 +17,27 @@ yo jhipster --force --no-insight
 ################################################################################
 # generate entities
 ################################################################################
-# for f in `ls .jhipster` ; do yo jhipster:entity ${f%.*} --force ; done
+for f in `ls .jhipster` ; do yo jhipster:entity ${f%.*} --force ; done
 # for f in `ls *.jh *.jdl` ; do yo jhipster:import-jdl ${f%} --force ; done
 
 # display app folder
 ls -al "$HOME"/app/
 
+yo jhipster:info
+
 ################################################################################
 # launch tests
 ################################################################################
-docker-compose -f src/main/docker/mysql.yml up -d
+docker-compose -f src/main/docker/postgresql.yml up -d
 
 ./mvnw clean test
 
 gulp test
 
-gulp eslint --no-notification
-
-./mvnw clean package -Pprod -DskipTests
+./mvnw -Pprod clean package -DskipTests
 docker ps -a
 
 java -jar target/*.war --spring.output.ansi.enabled=ALWAYS &
 sleep 60
 curl -v http://localhost:8080
+gulp itest
